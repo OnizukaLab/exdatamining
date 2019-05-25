c16d03c931e21dcc2903fcb0de3c127602a9c2e3
mt2tkm <sakaimatsumotot@gmail.com>
2019-05-26 01:25:59 +0900

    modify code

--- build.sbt
+++ build.sbt
@@ -1,10 +1,15 @@
 name := "udafApp"
 
-version := "0.1"
+version := "1.0"
 
 scalaVersion := "2.11.12"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.4.3",
-  "org.apache.spark" %% "spark-sql" % "2.4.3"
+  "org.apache.spark" %% "spark-sql" % "2.4.3",
+  "org.apache.spark" %% "spark-hive" % "2.2.0"
 )
+
+//libraryDependencies += "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+
+//libraryDependencies += "org.postgresql" % "postgresql" % "42.1.4"
--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,5 +1,35 @@
 package udafApp
 
-class Application {
+import scala.math.abs
+import scala.math._
 
+object Application {
+  // Search Global outlier Application
+  // Input  : Cube(All, Subsetdata)
+  // Output : Subsetdata,
+  def global_outlier(k: Int, all: Map[String, Seq[Double]], subset: Map[String, Map[String, Seq[Double]]]): List[List[(String, (Double, Double))]] = {
+    // 部分データ毎の処理
+    val subset_deviance = subset.map { case (key, mmap) =>
+      // 乖離度の計算
+      var deviance: (Double, Double) = (0D, 0D) //(Max, Min)
+
+      all.foreach { case (x_value, y_seq) =>
+        val subset_value = mmap.getOrElse(x_value, Seq(0.0, 0.0))
+        val dis_pattern = List(abs(y_seq.head - subset_value.head), abs(y_seq.head - subset_value(1)), abs(y_seq(1) - subset_value.head), abs(y_seq(1) - subset_value(1)))
+        deviance = (deviance._1 + dis_pattern.max, deviance._2 + dis_pattern.min)
+      }
+
+      key -> deviance
+    }.toList.sortBy(_._2).reverse
+
+    // 閾値の保持
+    val under_threshold: Double = subset_deviance(min(subset_deviance.size, k) - 1)._2._2
+    // 探索を行う部分データの取得
+    // subset_deviance.foreach(println)
+    // val a: List[(String,(Double,Double))] = subset_deviance
+    return List(
+      subset_deviance.filter(f => f._2._1 >= under_threshold),
+      subset_deviance.filter(f => f._2._1 < under_threshold)
+    ) //.map { f => f._1 }
+  }
 }
